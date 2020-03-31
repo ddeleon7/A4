948e8ef6e83189584cdbc8e61aa2cd22542b44d1
ddeleon7 <60410014+ddeleon7@users.noreply.github.com>
2020-03-30 19:26:11 -0700

    Add files via upload

---
+++ cs433main.cpp
@@ -0,0 +1,26 @@
+//#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>      
+//#include <pthread.h>     // Required for pthreads
+//#include <semaphore.h>	 
+#include <iostream>
+//#include <sys/wait.h>
+using namespace std;
+
+//parameters
+
+
+int main(int argc, char *argv[])
+{
+	cout << "---------------------------------------------------" << endl;
+	cout << "Authors: Dionisio de Leon and James Foerster" << endl;
+	cout << "CS433: Programming Assignment 4" << endl;
+	cout << "Due Date: 04/28/2020" << endl;
+	cout << "This program studies the behavior of concurrent" << endl;
+	cout << "threads using mutual exclusion locks, condition" << endl;
+	cout << "variables and semaphores. The implementation used" << endl;
+	cout << "is the producer and consumer buffer problem." << endl;
+	cout << "---------------------------------------------------" << endl << endl;
+
+    return 0;
+}
